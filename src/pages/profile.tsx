9f36d693d024dd6aa127773e7b5635b2e62524fc
Steele Shreve <94927484+steelesh@users.noreply.github.com>
2024-10-29 00:34:29 -0500

    add placeholder pages for profile and settings

---
+++ src/pages/profile.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Profile() {
+  return (
+    <>
+      <Head>
+        <title>UCollab — Profile</title>
+      </Head>
+      <h1>Profile</h1>
+    </>
+  );
+}
