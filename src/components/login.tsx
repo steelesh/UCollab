aed4b67f0d0b5294c1fe56036fd1aa726e6a8802
Steele Shreve <94927484+steelesh@users.noreply.github.com>
2024-10-29 02:10:34 -0500

    simplify

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,41 +1,43 @@
 export default function Login() {
   return (
-    <div className="my-16 flex justify-center">
-      <div className="card w-full max-w-sm bg-base-300 shadow-2xl">
-        <form className="card-body">
-          <div className="form-control">
-            <label htmlFor="email" className="label label-text">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              id="email"
-              type="email"
-              placeholder=""
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="password" className="label label-text">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              id="password"
-              type="password"
-              placeholder=""
-              className="input input-bordered"
-              required
-            />
-            <label className="label">
-              <a href="#" className="link-hover link label-text-alt">
-                Forgot password?
-              </a>
-            </label>
-          </div>
-          <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
-          </div>
-        </form>
+    <div className="flex w-full justify-center overflow-hidden rounded-lg">
+      <div className="my-16 flex justify-center">
+        <div className="card w-full max-w-sm bg-base-300 shadow-2xl">
+          <form className="card-body">
+            <div className="form-control">
+              <label htmlFor="email" className="label label-text">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                id="email"
+                type="email"
+                placeholder=""
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="password" className="label label-text">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                id="password"
+                type="password"
+                placeholder=""
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="link-hover link label-text-alt">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,7 @@ export default function Home() {
       <Head>
         <title>UCollab — Home</title>
       </Head>
-      <div className="flex w-full justify-center overflow-hidden rounded-lg shadow-lg">
-        <Login />
-      </div>
+      <Login />
     </>
   );
 }
