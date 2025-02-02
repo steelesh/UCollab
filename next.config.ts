35db3706e02b2a360f5fd82b771885da78a3fe8c
thomasza92 <127898797+thomasza92@users.noreply.github.com>
2025-02-01 20:01:59 -0500

    update next.config and prettier.config

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    typedRoutes: true,
-    authInterrupts: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.digitaloceanspaces.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "9000",
-        pathname: "/**",
-      },
-    ],
-  },
+    experimental: {
+        typedRoutes: true,
+        authInterrupts: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.digitaloceanspaces.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '9000',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ['prettier-plugin-tailwindcss'],
-  semi: true,
-  singleQuote: true,
-  bracketSpacing: true,
-  bracketSameLine: true,
-  printWidth: 120,
-  tabWidth: 4,
+    plugins: ['prettier-plugin-tailwindcss'],
+    semi: true,
+    singleQuote: true,
+    bracketSpacing: true,
+    bracketSameLine: true,
+    printWidth: 120,
+    tabWidth: 2,
 };
 
 export default config;
