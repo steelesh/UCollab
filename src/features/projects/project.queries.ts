0501868f798e6d3a982cd6c7cd4c0b97c99962c3
Steele Shreve <steele@steelesh.dev>
2025-03-15 14:23:02 -0500

    add query for getting projects

--- src/features/projects/project.queries.ts
+++ src/features/projects/project.queries.ts
@@ -1,10 +1,26 @@
 import { ProjectService } from './project.service';
 import type { Project, User } from '@prisma/client';
+import type { ExplorePageData, ProjectDetails } from './project.types';
 
 export const getProjectTitle = async (projectId: Project['id']) => {
   return ProjectService.getProjectTitle(projectId);
 };
 
-export const getRealTimeProject = async (projectId: Project['id'], userId: User['id']) => {
+export const getRealTimeProject = async (projectId: Project['id'], userId: User['id']): Promise<ProjectDetails> => {
   return ProjectService.getProjectById(projectId, userId);
+};
+
+export const getProjects = async (page = 1, limit = 12, userId: User['id']): Promise<ExplorePageData> => {
+  const [projects, totalCount] = await Promise.all([
+    ProjectService.getPaginatedProjects(page, limit, userId),
+    ProjectService.getProjectCount(userId),
+  ]);
+
+  return {
+    projects,
+    totalPages: Math.ceil(totalCount / limit),
+    currentPage: page,
+    limit,
+    totalCount,
+  };
 };
