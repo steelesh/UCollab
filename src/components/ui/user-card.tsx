017ee17642a455809d6f03ae6135ce7dfc2612ae
Steele Shreve <steele@steelesh.dev>
2025-03-29 10:48:26 -0500

    make user card consistent with new project card

--- src/components/ui/user-card.tsx
+++ src/components/ui/user-card.tsx
@@ -1,17 +1,15 @@
 import type { User } from "@prisma/client";
 
-import { MentorshipStatus } from "@prisma/client";
-import { ArrowUpRight, GraduationCap, Mail, UserCog } from "lucide-react";
+import { ArrowUpRight, GraduationCap, Mail } from "lucide-react";
 import Link from "next/link";
 
 import type { Technology } from "~/features/users/user.types";
-
-import { cn } from "~/lib/utils";
 
 import { Avatar, AvatarImage } from "./avatar";
 import { Badge } from "./badge";
 import { Card, CardContent, CardHeader } from "./card";
 import { Large } from "./large";
+import { MentorshipBadge } from "./mentorship-badges";
 import { Muted } from "./muted";
 import { Small } from "./small";
 import { TechnologyIcon } from "./technology-icon";
@@ -43,7 +41,7 @@ export function UserCard({ username, firstName, lastName, avatar, email, gradYea
               href={`/u/${username}`}
               className="group/link flex items-center gap-2 min-w-0"
             >
-              <Large noMargin className="truncate">
+              <Large noMargin className="truncate underline-offset-4 underline">
                 {firstName}
                 {" "}
                 {lastName}
@@ -71,31 +69,10 @@ export function UserCard({ username, firstName, lastName, avatar, email, gradYea
             </Small>
           </Muted>
         </div>
-        <Badge
-          variant="glossy"
-          className={cn(
-            "absolute top-1 right-1 flex items-center gap-1 text-xs",
-            mentorship === MentorshipStatus.MENTOR && "bg-blue-500/20 text-blue-400",
-            mentorship === MentorshipStatus.MENTEE && "bg-purple-500/20 text-purple-400",
-            mentorship === MentorshipStatus.NONE && "bg-neutral-500/20 text-neutral-400",
-          )}
-        >
-          {mentorship === MentorshipStatus.MENTOR && (
-            <Small noMargin className="flex items-center gap-1">
-              <UserCog className="w-3 h-3" />
-              {" "}
-              Mentor
-            </Small>
-          )}
-          {mentorship === MentorshipStatus.MENTEE && (
-            <Small noMargin className="flex items-center gap-1">
-              <GraduationCap className="w-3 h-3" />
-              {" "}
-              Mentee
-            </Small>
-          )}
-          {mentorship === MentorshipStatus.NONE && <Small noMargin>N/A</Small>}
-        </Badge>
+        <MentorshipBadge
+          status={mentorship}
+          className="absolute top-1 right-1 flex items-center gap-1 text-xs"
+        />
       </CardHeader>
       <CardContent>
         <div className="flex flex-wrap items-center gap-2">
