ff87e99993cb7f00c65b3361eca93c03901c4d7a
Steele Shreve <steele@steelesh.dev>
2025-04-03 22:48:22 -0500

    add strict csp

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://ucollab.blob.core.windows.net;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    connect-src 'self' https://ucollab.blob.core.windows.net;
+    worker-src 'self' blob:;
+    media-src 'self' blob: data: https://ucollab.blob.core.windows.net;
+    manifest-src 'self';
+    upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
   distDir: "build",
   output: "standalone",
@@ -12,6 +29,19 @@ const nextConfig: NextConfig = {
         pathname: "/ucollab-files/**",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
   },
 };
 
